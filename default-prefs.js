48115daa185794803076714cc4cff5267c0685ad
Michael Aaron Murphy <mmstick@pm.me>
2022-04-05 00:10:01 +0200

    fix: Lock prefs

--- default-prefs.js
+++ default-prefs.js
@@ -1,5 +1,5 @@
 // Copyright 2022 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
-pref("app.update.auto", false);
-pref("app.update.enabled", false);
+pref("app.update.auto", false, locked);
+pref("app.update.enabled", false, locked);
